12a4fb753e47f43ee5a7a3fcde54bc148b2c3e45
Anthony Edward <Anthony_ehab@hotmail.com>
2025-11-14 10:53:58 +0200

    Added Welcome.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,6 +5,7 @@ import './App.css'
 // IMPORT EL COMPONENT BETA3AK HNA EL AWL
 import AhlaComponent from './assets/components/AhlaComponent';
 import SessionSlots from './assets/components/SessionSlots';
+import Welcome from './assets/components/welcome';
 
 
 function App() {
@@ -16,6 +17,7 @@ function App() {
         {/* E3ML COPY-PASTE LEL LINE ELY T7T WE 7OT ESM EL COMPONENT BTA3K, WE 8AYAR EL PATH */}
         <Route path="/Ahla" element={<AhlaComponent />} />
         <Route path="/SessionSlots" element={<SessionSlots />} />
+        <Route path="/welcome" element={<Welcome />} />
 
 
       </Routes>
---
+++ client/src/assets/components/welcome.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+function welcome()
+{
+    return(
+        <h1>Welcome to our application!</h1>
+    );
+}
+
+export default welcome;

e144bd15cc056cc02de344d00ba2fa46d7c3bef6
Mohamed Amrallah <mohibamrallah@gmail.com>
2025-12-09 08:57:47 +0200

    Update students.js

--- backend/models/students.js
+++ backend/models/students.js
@@ -5,6 +5,7 @@ const studentsSchema = new mongoose.Schema({
     name: { type: String, required: true },
     email: { type: String, required: true },
     id: { type: Number, required: true },
+    status: { type: String, enum: ['present', 'absent', 'late','excused absence'], required: true },
 });
 
 module.exports = mongoose.model("students", studentsSchema);
